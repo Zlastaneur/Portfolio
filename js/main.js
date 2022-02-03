47a4588ab4140a3c85e2cb4a80c378040664c6b1
Stanley <stanley.defer@hotmail.Fr>
2022-02-03 05:40:34 +0100

    🚧 Projects card and animation (js)

--- js/main.js
+++ js/main.js
@@ -9,4 +9,29 @@ document.addEventListener("DOMContentLoaded", function () {
             header.classList.remove("sticky");
         }
     });
+
+    const openButton = document.querySelectorAll(".openButton");
+    console.log(openButton);
+
+    for (let i = 0; i < openButton.length; i++) {
+        openButton[i].addEventListener("click", openTheBox);
+    }
+
+    function openTheBox() {
+        const card = this.closest(".projectCard");
+        console.log(this);
+        const text = card.querySelector(".projectParagraph");
+        console.log(text);
+
+        console.log(card);
+        if (card.classList.contains("open") == false) {
+            console.log("Ouvert");
+            card.classList.add("open");
+            text.classList.add("projectParagraph_open");
+        } else {
+            console.log("Fermé");
+            card.classList.remove("open");
+            text.classList.remove("projectParagraph_open");
+        }
+    }
 });
